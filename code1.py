dbca8fe70559ccc6c421701faf228babe95f125a
Harisankar Karua <harishankark.hsk@gmail.com>
2016-02-09 01:33:51 +0530

    first commit

---
+++ code1.py
@@ -0,0 +1,36 @@
+import numpy
+import cv2
+
+    
+def combinations(iterable, r):
+    # combinations('ABCD', 2) --> AB AC AD BC BD CD
+    # combinations(range(4), 3) --> 012 013 023 123
+    pool = tuple(iterable)
+    n = len(pool)
+    if r > n:
+        return
+    indices = range(r)
+    yield tuple(pool[i] for i in indices)
+    while True:
+        for i in reversed(range(r)):
+            if indices[i] != i + n - r:
+                break
+        else:
+            return
+        indices[i] += 1
+        for j in range(i+1, r):
+            indices[j] = indices[j-1] + 1
+        yield tuple(pool[i] for i in indices)
+    
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    #code for checking output for single test input
+     fo = open("test_inputs/Test_input0.txt", "r")
